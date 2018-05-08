959b828e31dacc8f7133a309c0f6e28898cc3667
alexjackhughes <hughes@huckletree.com>
2018-05-08 16:44:20 +0100

    Add get todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,17 @@ app.post('/todos', (req, res) => {
   });
 });
 
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(4000, () => {
   console.log('Server has started on port 4000');
 });
+
+module.exports =  {app};
